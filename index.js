29847208fb44b658303ebcd43bd4bb5cf2121e14
Alex Merced <alex@alexmerced.dev>
2021-09-04 22:58:54 -0400

    better error handling

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 function createMatcher(patterns) {
-  return (v) => {
-    let result = [];
-    patterns.forEach(([pattern, callback], index) => {
-      if (eval(`Boolean(${pattern})`)) {
-        result.push(callback(v));
-      }
-    });
-    return result;
-  };
+  try {
+    return (v) => {
+      let result = [];
+      patterns.forEach(([pattern, callback], index) => {
+        if (eval(`Boolean(${pattern})`)) {
+          result.push(callback(v));
+        }
+      });
+      return result;
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function createSingleMatcher(patterns) {
-  return (v) => {
-    let result = [];
-    patterns.every(([pattern, callback], index) => {
-      if (eval(`Boolean(${pattern})`)) {
-        result.push(callback(v));
-        return false;
-      } else {
-        return true;
-      }
-    });
-    return result;
-  };
+  try {
+    return (v) => {
+      let result = [];
+      patterns.every(([pattern, callback], index) => {
+        if (eval(`Boolean(${pattern})`)) {
+          result.push(callback(v));
+          return false;
+        } else {
+          return true;
+        }
+      });
+      return result;
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function matchObject(obj, matcher) {
-  return Object.entries(obj).map((kv) => matcher(kv));
+  try {
+    return Object.entries(obj).map((kv) => matcher(kv));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function matchArray(arr, matcher) {
-  return arr.map((kv) => matcher(kv));
+  try {
+    return arr.map((kv) => matcher(kv));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = {
